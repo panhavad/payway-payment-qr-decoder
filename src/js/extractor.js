281ae9bb36d9995d0d946e36e3f6239c565b1b09
panhavad <panhavadd@gmail.com>
2021-05-09 02:53:55 +0700

    add todos

--- src/js/extractor.js
+++ src/js/extractor.js
@@ -1,3 +1,23 @@
-function extractClick() {
-	alert(1)
-}
+// @todo function to handle the submited data
+// @body handling the string, split anything to refine the string then input the next function
+
+
+
+// @todo handling epoch
+// @body convert the epch to time and make sure it can be concate to html, this might have to deal with string
+
+
+// @todo replacing the result text
+// @body this will handle the result display on the screen make sure everything is refresh and display correctly
+
+
+// @todo renovate the html result code
+// @body improve the display result by not using raw html, instead should use js to complie all of those list using clone function?
+
+
+// @todo update to QR scan
+// @body able to scan the qr and auto input that text from qr and execute it
+
+
+// @todo imtergrate the crc concate (next page?)
+// @body enable the feature to modify info and generate the crc for new QR 
